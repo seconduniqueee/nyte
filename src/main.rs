f09df3d25d49676d87b23376e89caf96c3679dcf
seconduniQuee <plotnikovfw@gmail.com>
2024-12-01 21:52:09 +0200

    Feat: handle basic events

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+mod window;
+
 use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
+use winit::event::{ElementState, WindowEvent};
 use winit::window::{Window, WindowId};
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::keyboard::{KeyCode};
+use winit::keyboard::PhysicalKey::Code;
 use winit::platform::windows::{Color, WindowExtWindows};
 
 #[derive(Default)]
 struct App {
     window: Option<Window>,
+    game_state: GameState,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -15,9 +20,39 @@ enum CustomEvent {
     Message(&'static str),
 }
 
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+    Other
+}
+
+struct GameState {
+    screen_color: (u8, u8, u8),
+}
+
+impl GameState {
+    fn update_screen_color(&mut self, screen_color: (u8, u8, u8)) -> &GameState {
+        self.screen_color =  screen_color;
+        self
+    }
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        GameState { screen_color: (0, 0, 255) }
+    }
+}
+
 impl ApplicationHandler<CustomEvent> for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+        let attrs = Window::default_attributes()
+            .with_title("Nyte Game Engine")
+            .with_maximized(true);
+
+        self.window = Some(event_loop.create_window(attrs).unwrap());
     }
 
     fn user_event(&mut self, event_loop: &ActiveEventLoop, event: CustomEvent) {
@@ -41,11 +76,69 @@ impl ApplicationHandler<CustomEvent> for App {
             WindowEvent::Resized(size) => {
                 println!("Resizing window: {:?}", size);
                 self.window.as_ref().unwrap().request_redraw();
-                self.window.as_ref().unwrap().set_border_color(Some(Color::from_rgb(255, 0, 0)));
             }
 
             WindowEvent::RedrawRequested => {
                 self.window.as_ref().unwrap().request_redraw();
+            }
+
+            WindowEvent::MouseInput {device_id, state, button} => {
+                println!("Device ID: {:?}", device_id);
+                println!("State: {:?}", state);
+                println!("Button: {:?}", button);
+            }
+
+            WindowEvent::KeyboardInput { event, .. } => {
+                if let ElementState::Released = event.state { return; }
+
+                let direction = match event.physical_key {
+                    Code(KeyCode::KeyS) |
+                    Code(KeyCode::ArrowDown) => Direction::Down,
+
+                    Code(KeyCode::KeyW) |
+                    Code(KeyCode::ArrowUp) => Direction::Up,
+
+                    Code(KeyCode::KeyD) |
+                    Code(KeyCode::ArrowLeft) => Direction::Left,
+
+                    Code(KeyCode::KeyA) |
+                    Code(KeyCode::ArrowRight) => Direction::Right,
+
+                    _ => Direction::Other,
+                };
+
+                match direction {
+                    Direction::Down => {
+                        self.game_state.update_screen_color((255, 0, 0));
+                    }
+                    Direction::Up => {
+                        self.game_state.update_screen_color((0, 255, 0));
+                    }
+                    Direction::Left => {
+                        self.game_state.update_screen_color((0, 0, 255));
+                    }
+                    Direction::Right => {
+                        self.game_state.update_screen_color((0, 0, 0));
+                    }
+                    _ => {
+                        self.game_state.update_screen_color((255, 255, 255));
+                    }
+                }
+
+                let screen_color = self.game_state.screen_color;
+                let color = Color::from_rgb(screen_color.0, screen_color.1, screen_color.2);
+
+                self.window
+                    .as_ref()
+                    .unwrap()
+                    .set_border_color(Some(color));
+
+                self.window
+                    .as_ref()
+                    .unwrap()
+                    .request_redraw();
+
+                println!("Result: {:?}", color)
             }
             _ => ()
         }
